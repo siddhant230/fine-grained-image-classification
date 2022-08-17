5d9cb1a08201c8dc73995792595f6e6f5f34626a
siddhant230 <rsiddhant73@gmail.com>
2022-08-18 00:28:39 +0530

    updated folder structure

--- src/models.py
+++ src/models.py
@@ -75,21 +75,6 @@ class Encoder_block(nn.Module):
         self.conv = nn.Conv2d(
             input_kernel_nums, output_kernel_nums, kernel_size)
         self.pool = nn.MaxPool2d(2, 2)
-        self.bn = nn.BatchNorm2d(100)
-
-    def forward(self, x):
-        x = self.conv(x)
-        x = self.pool(x)
-        x = self.bn(x)
-        return x
-
-
-class Encoder_block(nn.Module):
-    def __init__(self, input_kernel_nums, output_kernel_nums, kernel_size):
-        super(Encoder_block, self).__init__()
-        self.conv = nn.Conv2d(
-            input_kernel_nums, output_kernel_nums, kernel_size)
-        self.pool = nn.MaxPool2d(2, 2)
         self.bn = nn.BatchNorm2d(output_kernel_nums)
 
     def forward(self, x):
